8928bc4af600940b59095796fe063cf09a9968ea
mementum <mementum@users.noreply.github.com>
2015-01-20 22:27:47 +0100

    Added Limit, Close Orders to Broker and Expiry

--- backtrader/strategy.py
+++ backtrader/strategy.py
@@ -26,15 +26,18 @@ class MetaStrategy(LineIterator.__metaclass__):
 
     def dopreinit(cls, _obj, env, *args, **kwargs):
         _obj, args, kwargs = super(MetaStrategy, cls).dopreinit(_obj, *args, **kwargs)
-        _obj.env = env
         _obj._clock = env.datas[0]
         _obj._len = 0
         _obj._broker = env.brokers[0] if env.brokers else None
+        _obj.env = env
+        _obj.datas = env.datas
         return _obj, args[1:], kwargs
 
 
 class Strategy(LineIterator):
     __metaclass__ = MetaStrategy
+
+    extralines = 1
 
     def start(self):
         pass
@@ -44,11 +47,12 @@ class Strategy(LineIterator):
 
     # Need to simulate len and forwarding because systems have no "lines"
     # This may change in the future
-    def __len__(self):
-        return self._len
+    if True:
+        def __len__(self):
+            return self._len
 
-    def forward(self):
-        self._len += 1
+        def forward(self):
+            self._len += 1
 
     def setbroker(self, broker):
         self._broker = broker
@@ -62,11 +66,11 @@ class Strategy(LineIterator):
     def ordernotify(self, order):
         pass
 
-    def buy(self, data, size, price=None, how=None, valid=None):
-        self._broker.buy(self, data, size=size, price=price, how=how, valid=valid)
+    def buy(self, data, size, price=None, exectype=None, valid=None):
+        return self._broker.buy(self, data, size=size, price=price, exectype=exectype, valid=valid)
 
-    def sell(self, data, size, price=None, how=None, valid=None):
-        self._broker.sell(self, data, size=size, price=price, how=how, valid=valid)
+    def sell(self, data, size, price=None, exectype=None, valid=None):
+        return self._broker.sell(self, data, size=size, price=price, exectype=exectype, valid=valid)
 
     def position(self, data):
         return self._broker.getposition(data)
