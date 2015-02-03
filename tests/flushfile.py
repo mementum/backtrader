895356138e207a4f14ba9262163bb8383a5ba9c8
mementum <mementum@users.noreply.github.com>
2015-02-03 23:30:29 +0100

    StdOut/Error flushing under win32
    - StdOut/Errr flushing under win32 to avoid delayed line output
    - Added flushfile import to testbase to ensure all tests import it

---
+++ tests/flushfile.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+################################################################################
+#
+#  Copyright (C) 2015 Daniel Rodriguez
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+import sys
+
+class flushfile(object):
+
+    def __init__(self, f):
+        self.f = f
+
+    def write(self, x):
+        self.f.write(x)
+        self.f.flush()
+
+if sys.platform == 'win32':
+    sys.stdout = flushfile(sys.stdout)
+    sys.stderr = flushfile(sys.stderr)
