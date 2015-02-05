3e6d41b8a871308dcbd7bc2dbe1ec64100e92659
mementum <mementum@users.noreply.github.com>
2015-02-05 23:25:10 +0100

    Broker setcash and PositionSizer
    - Broker can now setcash after creation (mostly useful for automatic
    instantiated broker)
    - PositionSizer base and Fix scheme added and integrated into Strategy
    with automatic creation and use upon None

---
+++ backtrader/positionsizer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+################################################################################
+#
+# Copyright (C) 2015 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+import broker
+import metabase
+
+
+class PosSizerBase(object):
+    __metaclass__ = metabase.MetaParams
+
+    params = (('broker', None,),)
+
+    def getsizing(self, data, broker=None):
+        broker = broker or self.params.broker
+        return self._getsizing(broker.getcommissioninfo(data), broker.getcash())
+
+    def _getsizing(self, comminfo, cash):
+        raise NotImplementedError
+
+    def setbroker(self, broker):
+        self.params.broker = broker
+
+    def getbroker(self):
+        return self.params.broker
+
+
+class PosSizerFix(PosSizerBase):
+    params = (('stake', 1),)
+
+    def _getsizing(self, comminfo, cash):
+        return self.params.stake
