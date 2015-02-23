6cc5c9ca21002b348b1e14eae52ad304040d4270
mementum <mementum@users.noreply.github.com>
2015-02-23 16:11:19 +0100

    setup.py find_packages automatically
    - indicators and feeds subpackages not automatically included if not

--- setup.py
+++ setup.py
@@ -88,8 +88,8 @@ setuptools.setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    # packages=setup.find_packages(exclude=['docs', 'samples',]),
-    packages=['backtrader',],
+    packages=setuptools.find_packages(exclude=['docs', 'samples',]),
+    # packages=['backtrader', '],
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
