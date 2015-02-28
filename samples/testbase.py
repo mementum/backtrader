018695c813376390a7c69c6a39d105a1d36b794d
mementum <danjrod@gmail.com>
2015-02-28 20:45:32 +0100

    testbase inserts development path to sys.path rather than append

--- samples/testbase.py
+++ samples/testbase.py
@@ -24,4 +24,4 @@ import sys
 import flushfile
 
 # append module root directory to sys.path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
