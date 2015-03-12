f8dbdeff6ccafdc88a89371753c14edf74e1cd51
mementum <danjrod@gmail.com>
2015-03-12 12:15:53 +0100

    observers subpackage created
    - buysell observer
    - profit and loss observer
    - cash and value observer

---
+++ backtrader/observers/buysell.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+################################################################################
+#
+# Copyright (C) 2015 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import math
+
+from .. import lineiterator
+
+
+class BuySellObserver(object):
+    def __init__(self, datas):
+        for data in datas:
+            _BuySellObserver(data)
+
+
+class _BuySellObserver(lineiterator.LineObserver):
+    lines = ('buy', 'sell',)
+    subplot = False
+
+    plotinfo = dict(
+        _plotlinelabels=True,
+        buy=dict(marker='^', markersize=8.0, color='g', fillstyle='none'),
+        sell=dict(marker='v', markersize=8.0, color='r', fillstyle='none')
+    )
+
+    def __init__(self):
+        self.data = self.datas[0]
+
+    def next(self):
+        buy = list()
+        sell = list()
+
+        for order in self._owner._orderspending:
+            if order.data != self.data or not order.executed.size:
+                continue
+
+            if order.isbuy():
+                buy.append(order.executed.price)
+            else:
+                sell.append(order.executed.price)
+
+        # Write down the average buy/sell price
+        self.lines.buy = math.fsum(buy)/float(len(buy) or 'NaN')
+        self.lines.sell = math.fsum(sell)/float(len(sell) or 'NaN')
---
+++ backtrader/observers/cashvalue.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+################################################################################
+#
+# Copyright (C) 2015 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .. import lineiterator
+
+
+class CashObserver(lineiterator.LineObserver):
+    lines = ('cash',)
+
+    def next(self):
+        self.lines[0][0] = self._owner.broker.getcash()
+
+
+class ValueObserver(lineiterator.LineObserver):
+    lines = ('value',)
+
+    def next(self):
+        self.lines[0][0] = self._owner.broker.getvalue()
+
+
+class CashValueObserver(lineiterator.LineObserver):
+    lines = ('cash', 'value')
+
+    plotname = 'Cash/Market Value'
+
+    def next(self):
+        self.lines.cash = self._owner.broker.getcash()
+        self.lines.value = self._owner.broker.getvalue()
