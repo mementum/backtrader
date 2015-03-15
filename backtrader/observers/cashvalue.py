3b78764d0f932edc2ea860f9f21aec1bbbf57dd1
mementum <danjrod@gmail.com>
2015-03-15 20:08:11 +0100

    observers.cashvalue
    - drawdown and maxdrawdown
    - adapted to separated LineObserver

--- backtrader/observers/cashvalue.py
+++ backtrader/observers/cashvalue.py
@@ -21,28 +21,42 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from .. import lineiterator
+from .. import LineObserver
 
 
-class CashObserver(lineiterator.LineObserver):
+class CashObserver(LineObserver):
     lines = ('cash',)
 
     def next(self):
         self.lines[0][0] = self._owner.broker.getcash()
 
 
-class ValueObserver(lineiterator.LineObserver):
+class ValueObserver(LineObserver):
     lines = ('value',)
 
     def next(self):
         self.lines[0][0] = self._owner.broker.getvalue()
 
 
-class CashValueObserver(lineiterator.LineObserver):
+class CashValueObserver(LineObserver):
     lines = ('cash', 'value')
 
     plotinfo = dict(plotname='Cash/Market Value')
 
+    def __init__(self):
+        self.maxdrawdown = 0.0
+        self.peak = float('-inf')
+
     def next(self):
         self.lines.cash = self._owner.broker.getcash()
-        self.lines.value = self._owner.broker.getvalue()
+        self.lines.value = value = self._owner.broker.getvalue()
+
+        # update the maximum seen peak
+        if value > self.peak:
+            self.peak = value
+
+        # calculate the current drawdown
+        drawdown = 100.0 * (self.peak - value) / self.peak
+
+        # update the maxdrawdown if needed
+        self.maxdrawdown = max(self.maxdrawdown, drawdown)
