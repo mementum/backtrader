6b64e49f2d31bf3b3ca988cc9e2eb37a43f50338
mementum <danjrod@gmail.com>
2015-03-15 20:09:10 +0100

    backtrader packages import from observer

--- backtrader/__init__.py
+++ backtrader/__init__.py
@@ -26,6 +26,7 @@ from .lineseries import *
 from .lineiterator import *
 from .dataseries import *
 from .indicator import *
+from .observer import *
 from .strategy import *
 from .order import *
 from .comminfo import *
