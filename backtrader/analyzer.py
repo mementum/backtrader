1ab7c8e3b2fec0bd7b4715f2ef5da7e21fd218af
mementum <mementum@users.noreply.github.com>
2015-04-25 23:56:49 +0200

    analyzer keeps suboservers as variables

--- backtrader/analyzer.py
+++ backtrader/analyzer.py
@@ -23,14 +23,12 @@ from __future__ import (absolute_import, division, print_function,
 
 import six
 
-from .metabase import MetaParams
 from .observer import LineObserver
 from .observers import BuySellObserver
 from .observers import CashValueObserver
 from .observers import OperationsPnLObserver
 
 
-# class Analyzer(six.with_metaclass(MetaParams, object)):
 class Analyzer(LineObserver):
     plotinfo = dict(plot=False, plotskip=True)
 
@@ -41,6 +39,6 @@ class Analyzer(LineObserver):
     )
 
     def __init__(self, *args, **kwargs):
-        CashValueObserver(plot=self.params.plotcashvalue)
-        OperationsPnLObserver(plot=self.params.plotcashvalue)
-        BuySellObserver(plot=self.params.plotbuysell)
+        self.cashvalue = CashValueObserver(plot=self.params.plotcashvalue)
+        self.operations = OperationsPnLObserver(plot=self.params.plotcashvalue)
+        self.buysell = BuySellObserver(plot=self.params.plotbuysell)
