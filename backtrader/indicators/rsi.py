f2bec55161f7f9290ea1c5dce279462406240345
Daniel Rodriguez <danjrod@gmail.com>
2015-05-14 23:07:36 +0200

    Adapted indicators to new miscops naming and new functions module

--- backtrader/indicators/rsi.py
+++ backtrader/indicators/rsi.py
@@ -23,7 +23,7 @@ from __future__ import (absolute_import, division, print_function,
 
 from .. import Indicator
 from .ma import MovAv
-from .lineoperations import Max
+from .. import Max
 
 
 class UpDays(Indicator):
