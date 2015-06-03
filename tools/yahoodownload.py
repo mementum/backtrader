8e5770065c6b7f326d6b8b03ca7aa92d1d338b5f
mementum <mementum@users.noreply.github.com>
2015-06-03 23:58:05 +0200

    - Independent Yahoo Online Download Tool

---
+++ tools/yahoodownload.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2015 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+
+import argparse
+import collections
+import datetime
+import io
+import logging
+import sys
+
+
+import six
+from six.moves import urllib
+
+
+logging.basicConfig(
+    format='%(levelname)s: %(message)s',
+    level=logging.INFO)
+
+
+class YahooDownload(object):
+    baseurl = 'http://ichart.yahoo.com/table.csv?'
+
+    def __init__(self, ticker, fromdate, todate, period='d', reverse=True):
+
+        url = self.baseurl
+        url += 's=%s' % ticker
+        fromdate = fromdate
+        url += '&a=%d&b=%d&c=%d' % \
+               ((fromdate.month - 1), fromdate.day, fromdate.year)
+        todate = todate
+        url += '&d=%d&e=%d&f=%d' % \
+               ((todate.month - 1), todate.day, todate.year)
+        url += '&g=%s' % period
+        url += '&ignore=.csv'
+
+        self.datafile = urllib.request.urlopen(url)
+        if self.datafile.headers['Content-Type'] != 'text/csv':
+            raise ValueError(
+                'Wrong Content Type in headers %s' %
+                datafile.headers['Content-Type'])
+
+        self.headers = self.datafile.readline()  # skip the headers
+
+        # buffer everything from the socket into a local buffer
+        f = six.StringIO(self.datafile.read())
+        self.datafile.close()
+        self.datafile = f
+
+        if reverse:
+            # Yahoo data is in reverse order - reverse it
+            dq = collections.deque()
+            for line in self.datafile:
+                dq.appendleft(line)
+
+            f = six.StringIO()
+            f.writelines(dq)
+
+            self.datafile.close()
+            self.datafile = f
+
+    def writetofile(self, filename):
+        if not self.datafile:
+            return
+
+        if not hasattr(filename, 'read'):
+            # It's not a file - open it
+            f = open(filename, 'wb')
+        else:
+            f = filename
+
+        f.write(self.headers)
+
+        self.datafile.seek(0)
+        for line in self.datafile:
+            f.write(line)
+
+        f.close()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Download Yahoo CSV Finance Data')
+
+    parser.add_argument('--ticker', required=True,
+                        help='Ticker to be downloaded')
+
+    parser.add_argument('--notreverse', action='store_true', default=False,
+                        help='Do not reverse the downloaded files')
+
+    parser.add_argument('--timeframe', default='d',
+                        help='Timeframe to donwload (default d -> daily')
+
+    parser.add_argument('--fromdate', required=True,
+                        help='Starting date in YYYY-MM-DD format')
+
+    parser.add_argument('--todate', required=True,
+                        help='Ending date in YYYY-MM-DD format')
+
+    parser.add_argument('--outfile', required=True,
+                        help='Output file name')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    logging.info('Processing input parameters')
+    logging.info('Processing fromdate')
+    try:
+        fromdate = datetime.datetime.strptime(args.fromdate, '%Y-%m-%d')
+    except Exception as e:
+        logging.error('Converting fromdate failed')
+        logging.error(str(e))
+        sys.exit(1)
+
+    logging.info('Processing todate')
+    try:
+        todate = datetime.datetime.strptime(args.todate, '%Y-%m-%d')
+    except Exception as e:
+        logging.error('Converting todate failed')
+        logging.error(str(e))
+        sys.exit(1)
+
+    logging.info('Do Not Reverse flag status')
+    reverse = not args.notreverse
+
+    logging.info('Downloading from yahoo')
+    try:
+        yahoodown = YahooDownload(
+            ticker=args.ticker,
+            fromdate=fromdate,
+            todate=todate,
+            period=args.timeframe,
+            reverse=reverse)
+    except Exception as e:
+        logging.error('Downloading data from Yahoo failed')
+        logging.error(str(e))
+        sys.exit(1)
+
+    logging.info('Opening output file')
+    try:
+        ofile = open(args.outfile, 'wb')
+    except IOError as e:
+        logging.error('Error opening output file')
+        logging.error(str(e))
+        sys.exit(1)
+
+    logging.info('Writing downloaded data to output file')
+    try:
+        yahoodown.writetofile(ofile)
+    except Exception as e:
+        logging.error('Downloading data from Yahoo failed')
+        logging.error(str(e))
+        sys.exit(1)
+
+    logging.info('All operations completed successfully')
+    sys.exit(0)
