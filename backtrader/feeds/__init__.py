26d48463a5eea9a0ea32af23081ba9bef2939664
dro <dro>
2015-06-05 23:22:15 +0200

    Added own simple csv format for sample

--- backtrader/feeds/__init__.py
+++ backtrader/feeds/__init__.py
@@ -21,5 +21,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+from .btcsv import *
 from .vchartcsv import *
 from .yahoo import *
