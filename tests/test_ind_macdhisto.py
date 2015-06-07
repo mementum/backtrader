e5497e78178d2919f00ec4a7946cd9c50cde7276
dro <dro>
2015-06-07 22:43:09 +0200

    Addition of nosetest testcases covering indicators, data multi timeframe and resampling

---
+++ tests/test_ind_macdhisto.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2015 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import testcommon
+
+import backtrader as bt
+import backtrader.indicators as btind
+
+chkdatas = 1
+chkvals = [
+    [u'25.821368', u'32.469404', u'1.772445'],
+    [u'21.977853', u'26.469735', u'-2.845646'],
+    [u'3.843516', u'5.999669', u'4.618090'],
+]
+
+chkmin = 34
+chkind = btind.MACDHisto
+
+
+def test_run(main=False):
+    datas = [testcommon.getdata(i) for i in range(chkdatas)]
+    testcommon.runtest(datas,
+                       testcommon.TestStrategy,
+                       main=main,
+                       plot=main,
+                       chkind=chkind,
+                       chkmin=chkmin,
+                       chkvals=chkvals)
+
+
+if __name__ == '__main__':
+    test_run(main=True)
---
+++ tests/testcommon.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2015 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import testbase
+
+import datetime
+import os.path
+
+import backtrader as bt
+
+modpath = os.path.dirname(os.path.abspath(__file__))
+dataspath = '../samples/datas/sample'
+datafiles = [
+    '2006-day-001.txt',
+    '2006-week-001.txt',
+]
+
+DATAFEED = bt.feeds.BacktraderCSVData
+
+FROMDATE = datetime.datetime(2006, 1, 1)
+TODATE = datetime.datetime(2006, 12, 31)
+
+
+def getdata(index, fromdate=FROMDATE, todate=TODATE):
+
+    datapath = os.path.join(modpath, dataspath, datafiles[index])
+    data = DATAFEED(
+        dataname=datapath,
+        fromdate=fromdate,
+        todate=todate)
+
+    return data
+
+
+def runtest(datas, strategy, runonce=True, preload=True, plot=False, **kwargs):
+    cerebro = bt.Cerebro(runonce=runonce, preload=preload)
+
+    if isinstance(datas, bt.LineSeries):
+        datas = [datas]
+    for data in datas:
+        cerebro.adddata(data)
+
+    cerebro.addstrategy(strategy, **kwargs)
+    cerebro.run()
+    if plot:
+        cerebro.plot()
+
+
+class TestStrategy(bt.Strategy):
+    params = dict(main=False,
+                  chkind=[],
+                  chkmin=1,
+                  chkvals=None,
+                  chkargs=dict())
+
+    def __init__(self):
+        try:
+            ind = self.p.chkind[0]
+        except TypeError:
+            chkind = [self.p.chkind]
+        else:
+            chkind = self.p.chkind
+
+        self.ind = chkind[0](self.data, **self.p.chkargs)
+        for ind in chkind[1:]:
+            ind(self.data)
+
+        for data in self.datas[1:]:
+            chkind[0](data, **self.p.chkargs)
+
+            for ind in chkind[1:]:
+                ind(data)
+
+    def prenext(self):
+        pass
+
+    def nextstart(self):
+        self.chkmin = len(self)
+        super(TestStrategy, self).nextstart()
+
+    def next(self):
+        if self.p.main:
+            dtstr = self.data.datetime.date(0).strftime('%Y-%m-%d')
+            print('%s - %d - %f' % (dtstr, len(self), self.ind[0]))
+
+    def start(self):
+        pass
+
+    def stop(self):
+        l = len(self.ind)
+        mp = self.chkmin
+        chkpts = [0, -l + mp, (-l + mp) // 2]
+
+        if self.p.main:
+            print('----------------------------------------')
+            print('len ind %d == %d len self' % (l, len(self)))
+            print('minperiod %d' % self.chkmin)
+
+            print('chkpts are', chkpts)
+            for chkpt in chkpts:
+                dtstr = self.data.datetime.date(chkpt).strftime('%Y-%m-%d')
+                print('chkpt %d -> %s' % (chkpt, dtstr))
+
+            for lidx in range(self.ind.size()):
+                chkvals = list()
+                for chkpt in chkpts:
+                    chkvals.append('%f' % self.ind.lines[lidx][chkpt])
+
+                print('%s,' % chkvals)
+
+            print('vs')
+
+            for chkval in self.p.chkvals:
+                print(chkval)
+
+        else:
+            assert l == len(self)
+            assert mp == self.p.chkmin
+            for lidx, linevals in enumerate(self.p.chkvals):
+                for i, chkpt in enumerate(chkpts):
+                    chkval = '%f' % self.ind.lines[lidx][chkpt]
+                    assert chkval == linevals[i]
