2a6ac9f51db0b830b49d84e32ded38bb798b7809
mementum <mementum@users.noreply.github.com>
2015-06-07 22:43:09 +0200

    Addition of nosetest testcases covering indicators, data multi timeframe and resampling

---
+++ tests/test_ind_sumn.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2015 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import testcommon
+
+import backtrader as bt
+import backtrader.indicators as btind
+
+chkdatas = 1
+chkvals = [
+    [u'57406.490000', u'50891.010000', u'50424.690000'],
+]
+
+chkmin = 14
+chkind = btind.SumN
+chkargs = dict(period=14)
+
+
+def test_run(main=False):
+    datas = [testcommon.getdata(i) for i in range(chkdatas)]
+    testcommon.runtest(datas,
+                       testcommon.TestStrategy,
+                       main=main,
+                       plot=main,
+                       chkind=chkind,
+                       chkmin=chkmin,
+                       chkvals=chkvals,
+                       chkargs=chkargs)
+
+
+if __name__ == '__main__':
+    test_run(main=True)
