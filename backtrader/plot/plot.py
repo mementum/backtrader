be5a01beb54bcf3ed710923db8afc5a41d74cc81
dro <dro>
2015-06-16 10:39:16 +0200

    Plot support for lines having a name different than the class alias (ex: plusDI can be plotted as +DI)

--- backtrader/plot/plot.py
+++ backtrader/plot/plot.py
@@ -310,7 +310,7 @@ class Plot(six.with_metaclass(MetaParams, object)):
             if masterax and not ind.plotinfo.plotlinelabels:
                 label = indlabel * (lineidx == 0) or '_nolegend'
             else:
-                label = linealias
+                label = lineplotinfo._get('_name', '') or linealias
 
             # plot data
             lplot = line.plotrange(self.pinf.xstart, self.pinf.xend)
