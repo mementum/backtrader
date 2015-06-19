4aa5132a0a89a22a497dbe38f4d12356c984dc70
mementum <mementum@users.noreply.github.com>
2015-06-19 18:53:48 +0200

    Fully specified Python versions supported in setup.py and some PEP8 changes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; py-indent-offset:4 -*-
-################################################################################
+###############################################################################
 #
 # Copyright (C) 2015 Daniel Rodriguez
 #
@@ -17,10 +17,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################################
+###############################################################################
 import os.path
 import codecs  # To use a consistent encoding
 import setuptools
+
+import backtrader
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -29,35 +31,34 @@ with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 pname = 'backtrader'
-import backtrader
 pversion = backtrader.__version__
 gurl = 'https://github.com/mementum/' + pname
 gdurl = gurl + '/tarball/' + pversion
 
 setuptools.setup(
-    name = pname,
+    name=pname,
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version=pversion,
 
-    description = 'BackTesting Engine',
+    description='BackTesting Engine',
     long_description=long_description,
 
-     # The project's main homepage.
+    # The project's main homepage.
     url=gurl,
     download_url=gdurl,
 
     # Author details
-    author = 'Daniel Rodriguez',
-    author_email = 'danjrod@gmail.com',
+    author='Daniel Rodriguez',
+    author_email='danjrod@gmail.com',
 
     # Choose your license
     license='GPLv3+',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers = [
+    classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
@@ -73,34 +74,40 @@ setuptools.setup(
         'Topic :: Office/Business :: Financial',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        ('License :: OSI Approved :: ' +
+         'GNU General Public License v3 or later (GPLv3+'),
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
 
         # Operating Systems on which it runs
         'Operating System :: OS Independent',
     ],
 
     # What does your project relate to?
-    keywords = ['trading', 'development',],
+    keywords=['trading', 'development'],
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=setuptools.find_packages(exclude=['docs', 'samples',]),
+    packages=setuptools.find_packages(exclude=['docs', 'samples']),
     # packages=['backtrader', '],
 
-    # List run-time dependencies here.  These will be installed by pip when your
+    # List run-time dependencies here.
+    # These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['six',],
+    install_requires=['six'],
 
-    # List additional groups of dependencies here (e.g. development dependencies).
+    # List additional groups of dependencies here
+    # (e.g. development dependencies).
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require={
@@ -113,8 +120,8 @@ setuptools.setup(
     # package_data={'sample': ['package_data.dat'],},
 
     # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],
 
