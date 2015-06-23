2cd0228e0cb5b67cb0e2083010de03e0f29707e0
mementum <mementum@users.noreply.github.com>
2015-06-24 00:12:46 +0200

    Refactored and simplified envelope indicators

--- backtrader/indicators/envelope.py
+++ backtrader/indicators/envelope.py
@@ -25,7 +25,150 @@ from backtrader import Indicator
 from backtrader.indicators import MovAv
 
 
-class Envelope(Indicator):
+class _EnvelopeMixIn(object):
+    lines = ('top', 'bot',)
+    params = (('perc', 2.5),)
+    plotlines = dict(top=dict(_samecolor=True), bot=dict(_samecolor=True),)
+
+    def __init__(self):
+        # Mix-in & directly from object -> does not necessarily need super
+        # super(_EnvelopeMixIn, self).__init__()
+        perc = self.p.perc / 100.0
+
+        self.lines.top = self.line * (1.0 + perc)
+        self.lines.bot = self.line * (1.0 - perc)
+
+        super(_EnvelopeMixIn, self).__init__()
+
+
+class SMAEnvelope(MovAv.SMA, _EnvelopeMixIn):
+    '''SMAEnvelope
+
+    SimpleMovingAverage and envelope band separated "perc" from it
+
+    Formula:
+      - ma = SimpleMovingAverage
+      - top = ma * (1 + perc)
+      - bot = ma * (1 - perc)
+
+    See also:
+      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
+
+    Lines:
+      - ma
+      - top
+      - bot
+
+    Params:
+      - period (30): period to apply to the moving average
+      - perc (2.5): percentage to separate the envelope bands from the source
+    '''
+    pass
+
+
+class EMAEnvelope(MovAv.EMA, _EnvelopeMixIn):
+    '''EMAEnvelope
+
+    ExponentialMovingAverage and envelope band separated "perc" from it
+
+    Formula:
+      - ma = ExponentialMovingAverage
+      - top = ma * (1 + perc)
+      - bot = ma * (1 - perc)
+
+    See also:
+      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
+
+    Lines:
+      - ema
+      - top
+      - bot
+
+    Params:
+      - period (30): period to apply to the moving average
+      - perc (2.5): percentage to separate the envelope bands from the source
+    '''
+    pass
+
+
+class SMMAEnvelope(MovAv.SMMA, _EnvelopeMixIn):
+    '''SMMAEnvelope
+
+    SmoothingMovingAverage and envelope band separated "perc" from it
+
+    Formula:
+      - ma = SmoothingMovingAverage
+      - top = mid * (1 + perc)
+      - bot = mid * (1 - perc)
+
+    See also:
+      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
+
+    Lines:
+      - ma
+      - top
+      - bot
+
+    Params:
+      - perc (2.5): percentage to separate the envelope bands from the source
+      - period (30): period to apply to the moving average
+    '''
+    pass
+
+
+class WMAEnvelope(MovAv.WMA, _EnvelopeMixIn):
+    '''WMAEnvelope
+
+    WeightedMovingAverage and envelope band separated "perc" from it
+
+    Formula:
+      - ma = WeightedMovingAverage
+      - top = mid * (1 + perc)
+      - bot = mid * (1 - perc)
+
+    See also:
+      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
+
+    Lines:
+      - ma
+      - top
+      - bot
+
+    Params:
+      - perc (2.5): percentage to separate the envelope bands from the source
+      - period (30): period to apply to the moving average
+    '''
+    pass
+
+
+class KAMAEnvelope(MovAv.KAMA, _EnvelopeMixIn):
+    '''KAMAEnvelope
+
+    AdaptiveMovingAverage and envelope band separated "perc" from it
+
+    Formula:
+      - ma = AdaptiveMovingAverage
+      - top = mid * (1 + perc)
+      - bot = mid * (1 - perc)
+
+    See also:
+      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
+
+    Lines:
+      - ma
+      - top
+      - bot
+
+    Params:
+      - perc (2.5): percentage to separate the envelope bands from the source
+      - period (30): period to apply to the moving average
+      - fast (2): period for the fast adaptive exponential factor
+      - fast (30): period for the slow adaptive exponential factor
+    '''
+    pass
+
+
+class Envelope(Indicator, _EnvelopeMixIn):
     '''Envelope
 
     It creates envelopes bands separated from the source data by a given
@@ -47,183 +190,8 @@ class Envelope(Indicator):
     Params:
       - perc (2.5): percentage to separate the envelope bands from the source
     '''
-
-    lines = ('mid', 'top', 'bot',)
-    params = (('perc', 2.5),)
+    plotinfo = dict(subplot=False)
 
     def __init__(self):
-        perc = self.p.perc / 100.0
-
-        self._prepare()
-        data = self._envsource()
-
-        self.lines.mid = data
-        self.lines.top = data * (1.0 + perc)
-        self.lines.bot = data * (1.0 - perc)
-
-    def _prepare(self):
-        pass
-
-    def _envsource(self):
-        return self.data
-
-
-class _MAEnvelope(Envelope):
-    params = (('movav', MovAv.SMA), ('period', None),)
-    plotinfo = dict(subplot=False)
-    plotlines = dict(
-        mid=dict(ls='--'),
-        top=dict(_samecolor=True),
-        bot=dict(_samecolor=True),)
-
-    def _plotlabel(self):
-        plabels = [self.p.period, self.p.perc]
-        plabels += [self.p.movav] * self.p.notdefault('movav')
-        return plabels
-
-    def _prepare(self):
-        self.p.period = self.p.period or self.p.movav.params.period
-
-    def _envsource(self):
-        return self.p.movav(self.data, period=self.p.period)
-
-
-class SMAEnvelope(_MAEnvelope):
-    '''SMAEnvelope
-
-    SimpleMovingAverage and envelope band separated "perc" from it
-
-    Formula:
-      - mid = SimpleMovingAverage
-      - top = mid * (1 + perc)
-      - bot = mid * (1 - perc)
-
-    See also:
-      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
-
-    Lines:
-      - mid
-      - top
-      - bot
-
-    Params:
-      - perc (2.5): percentage to separate the envelope bands from the source
-      - period (30): period to apply to the moving average
-    '''
-    pass
-
-
-class EMAEnvelope(_MAEnvelope):
-    '''EMAEnvelope
-
-    ExponentialMovingAverage and envelope band separated "perc" from it
-
-    Formula:
-      - mid = ExponentialMovingAverage
-      - top = mid * (1 + perc)
-      - bot = mid * (1 - perc)
-
-    See also:
-      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
-
-    Lines:
-      - mid
-      - top
-      - bot
-
-    Params:
-      - perc (2.5): percentage to separate the envelope bands from the source
-      - period (30): period to apply to the moving average
-    '''
-    params = (('movav', MovAv.EMA),)
-
-
-class SMMAEnvelope(_MAEnvelope):
-    '''SMMAEnvelope
-
-    SmoothingMovingAverage and envelope band separated "perc" from it
-
-    Formula:
-      - mid = SmoothingMovingAverage
-      - top = mid * (1 + perc)
-      - bot = mid * (1 - perc)
-
-    See also:
-      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
-
-    Lines:
-      - mid
-      - top
-      - bot
-
-    Params:
-      - perc (2.5): percentage to separate the envelope bands from the source
-      - period (30): period to apply to the moving average
-    '''
-    params = (('movav', MovAv.SMMA),)
-
-
-class WMAEnvelope(_MAEnvelope):
-    '''WMAEnvelope
-
-    WeightedMovingAverage and envelope band separated "perc" from it
-
-    Formula:
-      - mid = WeightedMovingAverage
-      - top = mid * (1 + perc)
-      - bot = mid * (1 - perc)
-
-    See also:
-      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
-
-    Lines:
-      - mid
-      - top
-      - bot
-
-    Params:
-      - perc (2.5): percentage to separate the envelope bands from the source
-      - period (30): period to apply to the moving average
-    '''
-    params = (('movav', MovAv.WMA),)
-
-
-class KAMAEnvelope(_MAEnvelope):
-    '''KAMAEnvelope
-
-    AdaptiveMovingAverage and envelope band separated "perc" from it
-
-    Formula:
-      - mid = AdaptiveMovingAverage
-      - top = mid * (1 + perc)
-      - bot = mid * (1 - perc)
-
-    See also:
-      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_envelopes
-
-    Lines:
-      - mid
-      - top
-      - bot
-
-    Params:
-      - perc (2.5): percentage to separate the envelope bands from the source
-      - period (30): period to apply to the moving average
-      - fast (2): period for the fast adaptive exponential factor
-      - fast (30): period for the slow adaptive exponential factor
-    '''
-    params = (('movav', MovAv.KAMA), ('fast', None), ('slow', None),)
-
-    def _plotlabel(self):
-        plabels = [self.p.period, self.p.fast, self.p.slow, self.p.perc]
-        plabels += [self.p.movav] * self.p.notdefault('movav')
-        return plabels
-
-    def _prepare(self):
-        super(KAMAEnvelope)._prepare()
-        self.p.fast = self.p.fast or self.p.movav.params.fast
-        self.p.slow = self.p.slow or self.p.movav.params.slow
-
-    def _envsource(self):
-        return self.p.movav(self.data, period=self.p.period,
-                            fast=self.p.fast, slow=self.p.slow)
+        self.lines.mid = self.data
+        super(Envelope, self).__init__()
