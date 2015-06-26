12ec2e7a8810ef901df3fedf1eb57f3a1373a134
mementum <mementum@users.noreply.github.com>
2015-06-27 01:26:31 +0200

    Restored missing closing ) in License classifier in setup.py lost during PEP8 refactoring

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setuptools.setup(
 
         # Pick your license as you wish (should match "license" above)
         ('License :: OSI Approved :: ' +
-         'GNU General Public License v3 or later (GPLv3+'),
+         'GNU General Public License v3 or later (GPLv3+)'),
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
