5849e906190d03ffae69769ded33bc91c72228fe
dro <dro>
2015-06-27 01:20:44 +0200

    Oscillators and MovingAverage family oscillators added

--- tests/test_ind_oscillator.py
+++ tests/test_ind_oscillator.py
@@ -21,30 +21,37 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-# The modules below should/must define __all__ with the Indicator objects
-# of prepend an "_" (underscore) to private classes/variables
+import testcommon
 
-from .basicops import *
+import backtrader as bt
+import backtrader.indicators as btind
 
-# depends on basicops
-from .ma import *
+chkdatas = 1
+chkvals = [
+    ['56.477000', '51.185333', '2.386667']
+]
 
-# depends on moving averages
-from .deviation import *
+chkmin = 30
+chkind = btind.Oscillator
 
-# depend on basicops, moving averages and deviations
-from .atr import *
-from .aroon import *
-from .bollinger import *
-from .cci import *
-from .crossover import *
-from .dema import *
-from .dpo import *
-from .directionalmove import *
-from .envelope import *
-from .macd import *
-from .momentum import *
-from .oscillator import *
-from .rsi import *
-from .stochastic import *
-from .williams import *
+
+class TS2(testcommon.TestStrategy):
+    def __init__(self):
+        ind = btind.MovAv.SMA(self.data)
+        self.p.inddata = [ind]
+        super(TS2, self).__init__()
+
+
+def test_run(main=False):
+    datas = [testcommon.getdata(i) for i in range(chkdatas)]
+    testcommon.runtest(datas,
+                       TS2,
+                       main=main,
+                       plot=main,
+                       chkind=chkind,
+                       chkmin=chkmin,
+                       chkvals=chkvals)
+
+
+if __name__ == '__main__':
+    test_run(main=True)
