0f7767a95ade0949c4c367a9263f98996190d92e
dro <dro>
2015-06-27 01:20:55 +0200

    Test case for envelope

---
+++ tests/test_ind_envelope.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2015 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import testcommon
+
+import backtrader as bt
+import backtrader.indicators as btind
+
+chkdatas = 1
+chkvals = [
+    ['4063.463000', '3644.444667', '3554.693333'],
+    ['4165.049575', '3735.555783', '3643.560667'],
+    ['3961.876425', '3553.333550', '3465.826000']
+]
+
+chkmin = 30
+chkind = btind.Envelope
+
+
+class TS2(testcommon.TestStrategy):
+    def __init__(self):
+        ind = btind.MovAv.SMA(self.data)
+        self.p.inddata = [ind]
+        super(TS2, self).__init__()
+
+
+def test_run(main=False):
+    datas = [testcommon.getdata(i) for i in range(chkdatas)]
+    testcommon.runtest(datas,
+                       TS2,
+                       main=main,
+                       plot=main,
+                       chkind=chkind,
+                       chkmin=chkmin,
+                       chkvals=chkvals)
+
+
+if __name__ == '__main__':
+    test_run(main=True)
