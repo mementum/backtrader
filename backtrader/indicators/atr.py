9dc7372ba5e5f258c75da9239041dc3639efd540
dro <dro>
2015-07-01 00:47:08 +0200

    Indicators(79): TrueLow, TrueHigh added and TrueRange refactored to use them

--- backtrader/indicators/atr.py
+++ backtrader/indicators/atr.py
@@ -21,8 +21,49 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from backtrader import Indicator, Max, Min
-from . import MovAv
+from . import Indicator, Max, Min, MovAv
+
+
+class TrueHigh(Indicator):
+    '''
+    Defined by J. Welles Wilder, Jr. in 1978 in his book *"New Concepts in
+    Technical Trading Systems"* for the ATR
+
+    Records the "true high" which is the maximum of today's high and
+    yesterday's close
+
+    Formula:
+      - truehigh = max(high, close_prev)
+
+    See:
+      - http://en.wikipedia.org/wiki/Average_true_range
+    '''
+    lines = ('truehigh',)
+
+    def __init__(self):
+        self.lines.truehigh = Max(self.data.high, self.data.close(-1))
+        super(TrueHigh, self).__init__()
+
+
+class TrueLow(Indicator):
+    '''
+    Defined by J. Welles Wilder, Jr. in 1978 in his book *"New Concepts in
+    Technical Trading Systems"* for the ATR
+
+    Records the "true high" which is the maximum of today's high and
+    yesterday's close
+
+    Formula:
+      - truelow = min(high, close_prev)
+
+    See:
+      - http://en.wikipedia.org/wiki/Average_true_range
+    '''
+    lines = ('truelow',)
+
+    def __init__(self):
+        self.lines.truelow = Min(self.data.low, self.data.close(-1))
+        super(TrueLow, self).__init__()
 
 
 class TrueRange(Indicator):
@@ -48,10 +89,7 @@ class TrueRange(Indicator):
     lines = ('tr',)
 
     def __init__(self):
-        close1 = self.data.close(-1)
-        truehigh = Max(self.data.high, close1)
-        truelow = Min(self.data.low, close1)
-        self.lines.tr = truehigh - truelow
+        self.lines.tr = TrueHigh(self.data) - TrueLow(self.data)
         super(TrueRange, self).__init__()
 
 
