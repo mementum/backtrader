98b2d5185bf51fba445f7f88b6ffd4f6dcbdad97
dro <dro>
2015-07-02 23:23:42 +0200

    Added sphinx etension to automate documentation of indicators

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
@@ -32,6 +32,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx_btind',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
