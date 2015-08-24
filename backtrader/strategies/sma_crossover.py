fa1e1d31da8a5c62eca2bc94d447a2a2a8b5aa10
mementum <mementum@users.noreply.github.com>
2015-08-24 23:10:54 +0200

    SMA_CrossOver refactoring and documentation

--- backtrader/strategies/sma_crossover.py
+++ backtrader/strategies/sma_crossover.py
@@ -26,14 +26,42 @@ import backtrader as bt
 import backtrader.indicators as btind
 
 
-class SMA_CrossOver(bt.Strategy):
+class MA_CrossOver(bt.Strategy):
+    '''
+    This is a long-only strategy which operates on a moving average cross
 
-    params = (('fast', 10), ('slow', 30))
+    Note:
+      - Although the default
+
+    Buy Logic:
+      - No position is open on the data
+
+      - The ``fast`` strategy crosses over the ``slow`` strategy to the upside.
+
+    Sell Logic:
+      - A position exists on the data
+
+      - The ``fast`` strategy crosses over the ``slow`` strategy to the
+        downside
+
+    Order Execution Type:
+      - Market
+
+    '''
+    alias = ('SMA_CrossOver',)
+
+    params = (
+        # period for the fast Moving Average
+        ('fast', 10),
+        # period for the slow moving average
+        ('slow', 30),
+        # moving average to use
+        ('_movav', btind.MovAv.SMA)
+    )
 
     def __init__(self):
-
-        sma_fast = btind.SMA(period=self.p.fast)
-        sma_slow = btind.SMA(period=self.p.slow)
+        sma_fast = self.p._movav(period=self.p.fast)
+        sma_slow = self.p._movav(period=self.p.slow)
 
         self.buysig = btind.CrossOver(sma_fast, sma_slow)
 
