b8a59ebeba8d0b5c38942df94c6f86763c84fcc0
mementum <mementum@users.noreply.github.com>
2015-09-14 21:41:04 +0200

    Add SQN Analyzer

---
+++ backtrader/analyzers/sqn.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2015 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from backtrader import Analyzer
+from backtrader.mathsupport import average, standarddev
+from backtrader.utils import AutoOrderedDict
+
+
+class SQN(Analyzer):
+    alias = ('SystemQualityNumber',)
+
+    def start(self):
+        self.ret = AutoOrderedDict()
+        self.pnl = list()
+        self.count = 0
+
+    def notify_trade(self, trade):
+        if trade.status == trade.Closed:
+            self.pnl.append(trade.pnlcomm)
+            self.count += 1
+
+    def stop(self):
+        pnl_av = average(self.pnl)
+        pnl_stddev = standarddev(self.pnl)
+
+        trades_sqr = pow(len(self.pnl), 0.5)
+
+        sqn = trades_sqr * pnl_av / pnl_stddev
+
+        self.ret.sqn = sqn
+        self.ret.trades = self.count
+
+    def get_analysis(self):
+        return self.ret
