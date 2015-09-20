92336447ca2bfb79ad24559db341786201ce86bf
mementum <mementum@users.noreply.github.com>
2015-09-20 11:58:21 +0200

    Update setuptools.py to remove 2.6 and add 3.5

--- setup.py
+++ setup.py
@@ -80,12 +80,12 @@ setuptools.setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
 
         # Operating Systems on which it runs
         'Operating System :: OS Independent',
