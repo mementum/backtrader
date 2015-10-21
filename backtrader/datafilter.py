85b23531599e8f19957df5336e162f2042641609
mementum <mementum@users.noreply.github.com>
2015-10-21 23:01:23 +0200

    Doc update for datafilter

--- backtrader/datafilter.py
+++ backtrader/datafilter.py
@@ -75,10 +75,60 @@ class DataFilter(bt.AbstractDataBase):
         return False  # no more data from underlying source
 
 
-class SessionFilter(with_metaclass(metabase.MetaParams, object)):
+class SessionFilterSimple(with_metaclass(metabase.MetaParams, object)):
+    '''
+    This class can be applied to a data source as a filter and will filter out
+    intraday bars which fall outside of the regular session times (ie: pre/post
+    market data)
+
+    This is a "simple" filter and must NOT manage the stack of the data (passed
+    during init and __call__)
+
+    It needs no "last" method because it has nothing to deliver
+
+    Bar Management will be done by the SimpleFilterWrapper class made which is
+    added durint the DataBase.addfilter_simple call
+    '''
     def __init__(self, data):
         pass
 
     def __call__(self, data):
+        '''
+        Return Values:
+
+          - False: nothing to filter
+          - True: filter current bar (because it's not in the session times)
+        '''
         # Both ends of the comparison are in the session
         return not data.sessionstart <= data.datetime.tm(0) <= data.sessionend
+
+
+class SessionFilter(with_metaclass(metabase.MetaParams, object)):
+    '''
+    This class can be applied to a data source as a filter and will filter out
+    intraday bars which fall outside of the regular session times (ie: pre/post
+    market data)
+
+    This is a "non-simple" filter and must manage the stack of the data (passed
+    during init and __call__)
+
+    It needs no "last" method because it has nothing to deliver
+    '''
+    def __init__(self, data):
+        pass
+
+    def __call__(self, data):
+        '''
+        Return Values:
+
+          - False: data stream was not touched
+          - True: data stream was manipulated (bar outside of session times and
+          - removed)
+        '''
+        if data.sessionstart <= data.datetime.tm(0) <= data.sessionend:
+            # Both ends of the comparison are in the session
+            return False  # say the stream is untouched
+
+        # bar outside of the regular session times
+        data.backwards()  # remove bar from data stack
+        return True  # signal the data was manipulated
