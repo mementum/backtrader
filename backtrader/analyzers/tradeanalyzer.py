30cef6548608910715ddd70154cad13fe9525079
takumn <takumn@gmail.com>
2015-12-20 17:30:34 +0900

    Solve compatibility problem between sys.maxint of Python2 and sys.maxsize of Python3

--- backtrader/analyzers/tradeanalyzer.py
+++ backtrader/analyzers/tradeanalyzer.py
@@ -154,7 +154,10 @@ class TradeAnalyzer(Analyzer):
             ml = trades.len.max or 0
             trades.len.max = max(ml, trade.barlen)
 
-            ml = trades.len.min or sys.maxint
+            try:
+                ml = trades.len.min or sys.maxint
+            except AttributeError:
+                ml = trades.len.min or sys.maxsize
             trades.len.min = min(ml, trade.barlen)
 
             # Length Won/Lost
@@ -168,7 +171,10 @@ class TradeAnalyzer(Analyzer):
                 m = trwl.max or 0
                 trwl.max = max(m, trade.barlen * wl)
                 if trade.barlen * wl:
-                    m = trwl.min or sys.maxint
+                    try:
+                        m = trwl.min or sys.maxint
+                    except AttributeError:
+                        m = trwl.min or sys.maxsize
                     trwl.min = min(m, trade.barlen * wl)
 
             # Length Long/Short
@@ -185,7 +191,10 @@ class TradeAnalyzer(Analyzer):
                 # max/min
                 m = trls.max or 0
                 trls.max = max(m, barlen)
-                m = trls.min or sys.maxint
+                try:
+                    m = trls.min or sys.maxint
+                except AttributeError:
+                    m = trls.min or sys.maxsize
                 trls.min = min(m, barlen or m)
 
                 for wlname in ['won', 'lost']:
@@ -202,7 +211,10 @@ class TradeAnalyzer(Analyzer):
                     # max/min
                     m = trls_wl.max or 0
                     trls_wl.max = max(m, barlen2)
-                    m = trls_wl.min or sys.maxint
+                    try:
+                        m = trls_wl.min or sys.maxint
+                    except AttributeError:
+                        m = trls_wl.min or sys.maxsize
                     trls_wl.min = min(m, barlen2 or m)
 
     def get_analysis(self):
