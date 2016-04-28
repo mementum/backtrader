ca9577c5b253bbca60f3391af037b4ae6d126201
mementum <mementum@users.noreply.github.com>
2016-04-28 19:31:21 +0200

    PivotPoint sample. Addresses #81

---
+++ samples/pivot-point/ppsample.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2016 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import argparse
+
+import backtrader as bt
+import backtrader.feeds as btfeeds
+import backtrader.utils.flushfile
+
+from pivotpoint import PivotPoint, PivotPoint1
+
+
+class St(bt.Strategy):
+    params = (('usepp1', False),
+              ('plot_on_daily', False))
+
+    def __init__(self):
+        if self.p.usepp1:
+            self.pp = PivotPoint1(self.data1)
+        else:
+            self.pp = PivotPoint(self.data1)
+
+        if self.p.plot_on_daily:
+            self.pp.plotinfo.plotmaster = self.data0
+
+    def next(self):
+        txt = ','.join(
+            ['%04d' % len(self),
+             '%04d' % len(self.data0),
+             '%04d' % len(self.data1),
+             self.data.datetime.date(0).isoformat(),
+             '%.2f' % self.pp[0]])
+
+        print(txt)
+
+
+def runstrat():
+    args = parse_args()
+
+    cerebro = bt.Cerebro()
+    data = btfeeds.BacktraderCSVData(dataname=args.data)
+    cerebro.adddata(data)
+    cerebro.resampledata(data, timeframe=bt.TimeFrame.Months)
+
+    cerebro.addstrategy(St,
+                        usepp1=args.usepp1,
+                        plot_on_daily=args.plot_on_daily)
+    cerebro.run()
+    if args.plot:
+        cerebro.plot(style='bar')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description='Sample for pivot point and cross plotting')
+
+    parser.add_argument('--data', required=False,
+                        default='../../datas/2005-2006-day-001.txt',
+                        help='Data to be read in')
+
+    parser.add_argument('--usepp1', required=False, action='store_true',
+                        help='Have PivotPoint look 1 period backwards')
+
+    parser.add_argument('--plot', required=False, action='store_true',
+                        help=('Plot the result'))
+
+    parser.add_argument('--plot-on-daily', required=False, action='store_true',
+                        help=('Plot the indicator on the daily data'))
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    runstrat()
