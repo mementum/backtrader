83e31cad532cbdebe83462b479bcf790355a0844
mementum <mementum@users.noreply.github.com>
2016-05-05 22:19:02 +0200

    Update copyright year in docs

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'backtrader'
-copyright = u'2015, Daniel Rodriguez'
+copyright = u'2015, 2016 Daniel Rodriguez'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
