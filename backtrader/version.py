a1ae9ef23d82da2998c482741c55f7a9e11f4396
mementum <mementum@users.noreply.github.com>
2016-08-10 18:07:11 +0200

    Prepare Release 1.8.4.93

--- backtrader/version.py
+++ backtrader/version.py
@@ -22,6 +22,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 
-__version__ = '1.8.3.93'
+__version__ = '1.8.4.93'
 
 __btversion__ = tuple(int(x) for x in __version__.split('.'))
