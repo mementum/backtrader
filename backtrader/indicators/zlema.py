98d16b8f16ce6c82b4605fa63044fdbc076784b5
mementum <mementum@users.noreply.github.com>
2016-08-18 00:23:01 +0200

    Docstring correction

--- backtrader/indicators/zlema.py
+++ backtrader/indicators/zlema.py
@@ -26,8 +26,7 @@ from . import Indicator, MovingAverageBase, MovAv
 
 
 class ZeroLagExponentialMovingAverage(MovingAverageBase):
-    '''By John Ehlers and Ric Way
-
+    '''
     The zero-lag exponential moving average (ZLEMA) is a variation of the EMA
     which adds a momentum term aiming to reduce lag in the average so as to
     track current prices more closely.
@@ -37,7 +36,6 @@ class ZeroLagExponentialMovingAverage(MovingAverageBase):
       - zlema = ema(2 * data - data(-lag))
 
     See also:
-      - http://www.mesasoftware.com/papers/ZeroLag.pdf
       - http://user42.tuxfamily.org/chart/manual/Zero_002dLag-Exponential-Moving-Average.html
 
     '''
