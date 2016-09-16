75725818808e3824ecfb2f14b6a48786f254f552
mementum <mementum@users.noreply.github.com>
2016-09-16 23:14:24 +0200

    Add multidata sample for unaligned data

---
+++ samples/multidata-strategy/multidata-strategy-unaligned.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2015, 2016 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import argparse
+import datetime
+
+# The above could be sent to an independent module
+import backtrader as bt
+import backtrader.feeds as btfeeds
+import backtrader.indicators as btind
+
+
+class MultiDataStrategy(bt.Strategy):
+    '''
+    This strategy operates on 2 datas. The expectation is that the 2 datas are
+    correlated and the 2nd data is used to generate signals on the 1st
+
+      - Buy/Sell Operationss will be executed on the 1st data
+      - The signals are generated using a Simple Moving Average on the 2nd data
+        when the close price crosses upwwards/downwards
+
+    The strategy is a long-only strategy
+    '''
+    params = dict(
+        period=15,
+        stake=10,
+        printout=True,
+    )
+
+    def log(self, txt, dt=None):
+        if self.p.printout:
+            dt = dt or self.data.datetime[0]
+            dt = bt.num2date(dt)
+            print('%s, %s' % (dt.isoformat(), txt))
+
+    def notify_order(self, order):
+        if order.status in [bt.Order.Submitted, bt.Order.Accepted]:
+            return  # Await further notifications
+
+        if order.status == order.Completed:
+            if order.isbuy():
+                buytxt = 'BUY COMPLETE, %.2f' % order.executed.price
+                self.log(buytxt, order.executed.dt)
+            else:
+                selltxt = 'SELL COMPLETE, %.2f' % order.executed.price
+                self.log(selltxt, order.executed.dt)
+
+        elif order.status in [order.Expired, order.Canceled, order.Margin]:
+            self.log('%s ,' % order.Status[order.status])
+            pass  # Simply log
+
+        # Allow new orders
+        self.orderid = None
+
+    def __init__(self):
+        # To control operation entries
+        self.orderid = None
+
+        # Create SMA on 2nd data
+        sma = btind.MovAv.SMA(self.data1, period=self.p.period)
+        # Create a CrossOver Signal from close an moving average
+        self.signal = btind.CrossOver(self.data1.close, sma)
+
+    def next(self):
+        if self.orderid:
+            return  # if an order is active, no new orders are allowed
+
+        if self.p.printout:
+            print('Self  len:', len(self))
+            print('Data0 len:', len(self.data0))
+            print('Data1 len:', len(self.data1))
+            print('Data0 len == Data1 len:',
+                  len(self.data0) == len(self.data1))
+
+            print('Data0 dt:', self.data0.datetime.datetime())
+            print('Data1 dt:', self.data1.datetime.datetime())
+
+        if not self.position:  # not yet in market
+            if self.signal > 0.0:  # cross upwards
+                self.log('BUY CREATE , %.2f' % self.data1.close[0])
+                self.buy(size=self.p.stake)
+
+        else:  # in the market
+            if self.signal < 0.0:  # crosss downwards
+                self.log('SELL CREATE , %.2f' % self.data1.close[0])
+                self.sell(size=self.p.stake)
+
+    def stop(self):
+        print('==================================================')
+        print('Starting Value - %.2f' % self.broker.startingcash)
+        print('Ending   Value - %.2f' % self.broker.getvalue())
+        print('==================================================')
+
+
+def runstrategy():
+    args = parse_args()
+
+    # Create a cerebro
+    cerebro = bt.Cerebro()
+
+    # Get the dates from the args
+    fromdate = datetime.datetime.strptime(args.fromdate, '%Y-%m-%d')
+    todate = datetime.datetime.strptime(args.todate, '%Y-%m-%d')
+
+    # Create the 1st data
+    data0 = btfeeds.YahooFinanceCSVData(
+        dataname=args.data0,
+        fromdate=fromdate,
+        todate=todate)
+
+    # Add the 1st data to cerebro
+    cerebro.adddata(data0)
+
+    # Create the 2nd data
+    data1 = btfeeds.YahooFinanceCSVData(
+        dataname=args.data1,
+        fromdate=fromdate,
+        todate=todate)
+
+    # Add the 2nd data to cerebro
+    cerebro.adddata(data1)
+
+    # Add the strategy
+    cerebro.addstrategy(MultiDataStrategy,
+                        period=args.period,
+                        stake=args.stake)
+
+    # Add the commission - only stocks like a for each operation
+    cerebro.broker.setcash(args.cash)
+
+    # Add the commission - only stocks like a for each operation
+    cerebro.broker.setcommission(commission=args.commperc)
+
+    # And run it
+    cerebro.run(runonce=not args.runnext,
+                preload=not args.nopreload,
+                oldsync=args.oldsync)
+
+    # Plot if requested
+    if args.plot:
+        cerebro.plot(numfigs=args.numfigs, volume=False, zdown=False)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='MultiData Strategy')
+
+    parser.add_argument('--data0', '-d0',
+                        default='../../datas/orcl-2003-2005.txt',
+                        help='1st data into the system')
+
+    parser.add_argument('--data1', '-d1',
+                        default='../../datas/yhoo-2003-2005.txt',
+                        help='2nd data into the system')
+
+    parser.add_argument('--fromdate', '-f',
+                        default='2003-01-01',
+                        help='Starting date in YYYY-MM-DD format')
+
+    parser.add_argument('--todate', '-t',
+                        default='2005-12-31',
+                        help='Starting date in YYYY-MM-DD format')
+
+    parser.add_argument('--period', default=15, type=int,
+                        help='Period to apply to the Simple Moving Average')
+
+    parser.add_argument('--cash', default=100000, type=int,
+                        help='Starting Cash')
+
+    parser.add_argument('--runnext', action='store_true',
+                        help='Use next by next instead of runonce')
+
+    parser.add_argument('--nopreload', action='store_true',
+                        help='Do not preload the data')
+
+    parser.add_argument('--oldsync', action='store_true',
+                        help='Use old data synchronization method')
+
+    parser.add_argument('--commperc', default=0.005, type=float,
+                        help='Percentage commission (0.005 is 0.5%%')
+
+    parser.add_argument('--stake', default=10, type=int,
+                        help='Stake to apply in each operation')
+
+    parser.add_argument('--plot', '-p', action='store_true',
+                        help='Plot the read data')
+
+    parser.add_argument('--numfigs', '-n', default=1,
+                        help='Plot using numfigs figures')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    runstrategy()
