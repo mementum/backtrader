0a040b0a835af9564318f3fc68f28785aa199e8a
mementum <mementum@users.noreply.github.com>
2016-10-19 19:34:21 +0200

    remove sizer from sigmacross2

--- samples/sigsmacross/sigsmacross2.py
+++ samples/sigsmacross/sigsmacross2.py
@@ -37,8 +37,6 @@ data0 = bt.feeds.YahooFinanceData(dataname='YHOO',
                                   fromdate=datetime(2011, 1, 1),
                                   todate=datetime(2012, 12, 31))
 
-cerebro.addsizer(bt.sizers.FixedSize, stake=100)
-
 cerebro.adddata(data0)
 
 cerebro.run()
