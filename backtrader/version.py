72e1342fce3ca2acbd4c4379cd796f744cd2c73b
mementum <mementum@users.noreply.github.com>
2017-01-31 11:49:51 +0100

    Release 1.9.26.105

--- backtrader/version.py
+++ backtrader/version.py
@@ -22,6 +22,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 
-__version__ = '1.9.25.105'
+__version__ = '1.9.26.105'
 
 __btversion__ = tuple(int(x) for x in __version__.split('.'))
