284f1d8139807d56abe02eae3daabe21e5e359fd
mementum <mementum@users.noreply.github.com>
2017-02-12 11:58:06 +0100

    Local fsum lookup and docstring cosmetics

--- backtrader/indicators/percentrank.py
+++ backtrader/indicators/percentrank.py
@@ -21,7 +21,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import math
+from math import fsum
 
 from . import BaseApplyN
 
@@ -31,12 +31,12 @@ __all__ = ['PercentRank', 'PctRank']
 
 class PercentRank(BaseApplyN):
     '''
-      Measures the percent rank of the current value with respect to that
-      of period bars ago
+    Measures the percent rank of the current value with respect to that of
+    period bars ago
     '''
     alias = ('PctRank',)
     lines = ('pctrank',)
     params = (
         ('period', 50),
-        ('func', lambda d: math.fsum(x < d[-1] for x in d) / len(d)),
+        ('func', lambda d: fsum(x < d[-1] for x in d) / len(d)),
     )
