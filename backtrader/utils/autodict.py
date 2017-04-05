2305f708e66b1876281e8617c430827ac13ce5f1
mementum <mementum@users.noreply.github.com>
2017-04-05 23:34:13 +0200

    Use dict internals for private attributes

--- backtrader/utils/autodict.py
+++ backtrader/utils/autodict.py
@@ -39,6 +39,8 @@ class AutoDictList(dict):
 class DotDict(dict):
     # If the attribut is not found in the usual places try the dict itself
     def __getattr__(self, key):
+        if key.startswith('__'):
+            return super(DotDict, self).__getattr__(key)
         return self[key]
 
 
