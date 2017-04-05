4d8c8751216d043763075f20df1869a7913f426a
mementum <mementum@users.noreply.github.com>
2017-04-05 22:07:48 +0200

    Ensure name is given even if plotinfo has no default for it

--- backtrader/lineseries.py
+++ backtrader/lineseries.py
@@ -452,8 +452,8 @@ class LineSeries(with_metaclass(MetaLineSeries, LineMultiple)):
             for i, sublabel in enumerate(sublabels):
                 # if isinstance(sublabel, LineSeries): ## DOESN'T WORK ???
                 if hasattr(sublabel, 'plotinfo'):
-                    sublabels[i] = sublabel.plotinfo.plotname or \
-                        sublabel.__name__
+                    sublabels[i] = (sublabel.plotinfo._get('plotname', '') or
+                                    sublabel.__name__)
             label += ' (%s)' % ', '.join(map(str, sublabels))
         return label
 
