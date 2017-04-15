2d737a9c51cb8f99f3fceed8a85d623f86d61fef
mementum <mementum@users.noreply.github.com>
2017-04-16 00:28:37 +0200

    Complete trading calendar specification and implementation

--- backtrader/tradingcal.py
+++ backtrader/tradingcal.py
@@ -22,10 +22,11 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 
-from datetime import timedelta, time
+from datetime import datetime, timedelta, time
 
 from .metabase import MetaParams
 from backtrader.utils.py3 import string_types, with_metaclass
+from backtrader.utils import UTC
 
 __all__ = ['TradingCalendarBase', 'TradingCalendar', 'PandasMarketCalendar']
 
@@ -41,6 +42,7 @@ MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY = range(7)
 WEEKEND = [SATURDAY, SUNDAY]
 ISOWEEKEND = [ISOSATURDAY, ISOSUNDAY]
 ONEDAY = timedelta(days=1)
+ONEYEAR = timedelta(days=365)
 
 
 class TradingCalendarBase(with_metaclass(MetaParams, object)):
@@ -59,38 +61,6 @@ class TradingCalendarBase(with_metaclass(MetaParams, object)):
         for the given ``date`` (``datetime/date`` instance)
         '''
         raise NotImplementedError
-
-
-class TradingCalendar(TradingCalendarBase):
-    params = (
-        ('open', time.min),
-        ('close', _time_max),
-        ('holidays', []),  # list of non trading days (date)
-        ('earlydays', []),  # list of tuples (date, opentime, closetime)
-        ('offdays', ISOWEEKEND),  # list of non trading (isoweekdays)
-    )
-
-    def schedule(self, day):
-        '''
-        Returns a tuple with the opening and closing times (``datetime.time``)
-        for the given ``date`` (``datetime/date`` instance)
-        '''
-        pass
-
-    def _nextday(self, day):
-        '''
-        Returns the next trading day (datetime/date instance) after ``day``
-        (datetime/date instance) and the isocalendar components
-
-        The return value is a tuple with 2 components: (nextday, (y, w, d))
-        '''
-        while True:
-            day += ONEDAY
-            isocal = day.isocalendar()
-            if isocal[2] in self.p.offdays or day in self.p.holidays():
-                continue
-
-            return day, isocal
 
     def nextday(self, day):
         '''
@@ -115,18 +85,36 @@ class TradingCalendar(TradingCalendarBase):
         # a week change even if the number is smaller (year change)
         return day.isocalendar()[1] != self._nextday(day)[1][1]
 
+    def last_monthday(self, day):
+        '''
+        Returns ``True`` if the given ``day`` (datetime/date) instance is the
+        last trading day of this month
+        '''
+        # Next day must be greater than day. If the week changes is enough for
+        # a week change even if the number is smaller (year change)
+        return day.month != self._nextday(day)[0].month
 
-class PandasMarketCalendar(TradingCalendarBase):
+    def last_yearday(self, day):
+        '''
+        Returns ``True`` if the given ``day`` (datetime/date) instance is the
+        last trading day of this month
+        '''
+        # Next day must be greater than day. If the week changes is enough for
+        # a week change even if the number is smaller (year change)
+        return day.year != self._nextday(day)[0].year
+
+
+class TradingCalendar(TradingCalendarBase):
     params = (
-        ('calendar', None),  # A pandas_market_calendar instance or exch name
+        ('open', time.min),
+        ('close', _time_max),
+        ('holidays', []),  # list of non trading days (date)
+        ('earlydays', []),  # list of tuples (date, opentime, closetime)
+        ('offdays', ISOWEEKEND),  # list of non trading (isoweekdays)
     )
 
     def __init__(self):
-        self._calendar = self.p.calendar
-
-        if isinstance(self._calendar, string_types):  # use passed mkt name
-            import pandas_market_calendar as mcal
-            self._calendar = mcal.get_calendar(self._calendar)
+        self._earlydays = [x[0] for x in self.p.earlydays]  # speed up searches
 
     def _nextday(self, day):
         '''
@@ -137,6 +125,116 @@ class PandasMarketCalendar(TradingCalendarBase):
         '''
         while True:
             day += ONEDAY
-            valid = self._calendar.valid_days(day, day)
-            if valid:  # has something
-                return valid.to_pydatetime()[0]
+            isocal = day.isocalendar()
+            if isocal[2] in self.p.offdays or day in self.p.holidays:
+                continue
+
+            return day, isocal
+
+    def schedule(self, day, tz=None):
+        '''
+        Returns the opening and closing times for the given ``day``. If the
+        method is called, the assumption is that ``day`` is an actual trading
+        day
+
+        The return value is a tuple with 2 components: opentime, closetime
+        '''
+        while True:
+            dt = day.date()
+            try:
+                i = self._earlydays.index(dt)
+                o, c = self.p.earlydays[i][1:]
+            except ValueError:  # not found
+                o, c = self.p.open, self.p.close
+
+            closing = datetime.combine(dt, c)
+            if tz is not None:
+                closing = tz.localize(closing).astimezone(UTC)
+                closing = closing.replace(tzinfo=None)
+
+            if day > closing:  # current time over eos
+                day += ONEDAY
+                continue
+
+            opening = datetime.combine(dt, o)
+            if tz is not None:
+                opening = tz.localize(opening).astimezone(UTC)
+                opening = opening.replace(tzinfo=None)
+
+            return opening, closing
+
+
+class PandasMarketCalendar(TradingCalendarBase):
+    '''
+    Wrapper of ``pandas_market_calendars`` for a trading calendar. The package
+    ``pandas_market_calendar`` must be installed
+
+    The param ``calendar`` accepts the following:
+
+      - string: the name of one of the calendars supported, for example
+        `NYSE`. The wrapper will attempt to get a calendar instance
+
+      - calendar instance: as returned by ``get_calendar('NYSE')``
+
+    See also:
+
+      - https://github.com/rsheftel/pandas_market_calendars
+
+      - http://pandas-market-calendars.readthedocs.io/
+
+    '''
+    params = (
+        ('calendar', None),  # A pandas_market_calendars instance or exch name
+    )
+
+    def __init__(self):
+        self._calendar = self.p.calendar
+
+        if isinstance(self._calendar, string_types):  # use passed mkt name
+            import pandas_market_calendars as mcal
+            self._calendar = mcal.get_calendar(self._calendar)
+
+        import pandas as pd  # guaranteed because of pandas_market_calendars
+        self.dcache = pd.DatetimeIndex([0.0])
+        self.idcache = pd.DataFrame(index=pd.DatetimeIndex([0.0]))
+
+    def _nextday(self, day):
+        '''
+        Returns the next trading day (datetime/date instance) after ``day``
+        (datetime/date instance) and the isocalendar components
+
+        The return value is a tuple with 2 components: (nextday, (y, w, d))
+        '''
+        day += ONEDAY
+        while True:
+            i = self.dcache.searchsorted(day)
+            if i == len(self.dcache):
+                # keep a cache of 1 year to speed up searching
+                self.dcache = self._calendar.valid_days(day, day + ONEYEAR)
+                continue
+
+            d = self.dcache[i].to_pydatetime()
+            return d, d.isocalendar()
+
+    def schedule(self, day, tz=None):
+        '''
+        Returns the opening and closing times for the given ``day``. If the
+        method is called, the assumption is that ``day`` is an actual trading
+        day
+
+        The return value is a tuple with 2 components: opentime, closetime
+        '''
+        while True:
+            i = self.idcache.index.searchsorted(day.date())
+            if i == len(self.idcache):
+                # keep a cache of 1 year to speed up searching
+                self.idcache = self._calendar.schedule(day, day + ONEYEAR)
+                continue
+
+            st = (x.tz_localize(None) for x in self.idcache.iloc[i, 0:2])
+            opening, closing = st  # Get utc naive times
+            if day > closing:  # passed time is over the sessionend
+                day += ONEDAY  # wrap over to next day
+                continue
+
+            return opening.to_pydatetime(), closing.to_pydatetime()
