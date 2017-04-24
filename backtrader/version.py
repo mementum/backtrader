d07a3dc67ab933288b13b6c4fb078d205499b3d3
mementum <mementum@users.noreply.github.com>
2017-04-24 22:08:22 +0200

    Increase version to 44

--- backtrader/version.py
+++ backtrader/version.py
@@ -22,6 +22,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 
-__version__ = '1.9.43.116'
+__version__ = '1.9.44.116'
 
 __btversion__ = tuple(int(x) for x in __version__.split('.'))
