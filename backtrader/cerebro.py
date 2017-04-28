4993712f5a826650671211127f9211b3b08f6c01
Dimitri John Ledkov <xnox@ubuntu.com>
2017-04-28 23:41:34 +0100

    Allow saving figures, instead of showing them.
    For example, one can pass --plot savefig=True to bt-run script to
    store all figures as backtrader-plot-N.pdf files. Useful for running
    backtests non-interactively and saving plots.
    
    Currently stores as PDF, but other naming pattern and file extensions
    can be used (e.g. 'plot-%i.png'). Current format, figure size, and dpi
    optimised for widescreen on-screen viewing.

--- backtrader/cerebro.py
+++ backtrader/cerebro.py
@@ -693,6 +693,7 @@ class Cerebro(with_metaclass(MetaParams, object)):
     broker = property(getbroker, setbroker)
 
     def plot(self, plotter=None, numfigs=1, iplot=True, start=None, end=None,
+             savefig=False, figfilename='backtrader-plot-%i.pdf',
              **kwargs):
         '''
         Plots the strategies inside cerebro
@@ -740,7 +741,10 @@ class Cerebro(with_metaclass(MetaParams, object)):
 
                 figs.append(rfig)
 
-            plotter.show()
+            if savefig:
+                plotter.savefig(figfilename % len(figs))
+            else:
+                plotter.show()
         return figs
 
     def __call__(self, iterstrat):
