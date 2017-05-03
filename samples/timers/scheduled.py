b3d58955f5dbdf474a5ee1ca0aac7d6cd2c75ab0
mementum <mementum@users.noreply.github.com>
2017-05-03 22:18:50 +0200

    Add timers samples

---
+++ samples/timers/scheduled.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2017 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import argparse
+import datetime
+
+import backtrader as bt
+
+
+class St(bt.Strategy):
+    params = dict(
+        when=bt.timer.SESSION_START,
+        timer=True,
+        cheat=False,
+        offset=datetime.timedelta(),
+        repeat=datetime.timedelta(),
+        weekdays=[],
+    )
+
+    def __init__(self):
+        bt.ind.SMA()
+        if self.p.timer:
+            self.add_timer(
+                when=self.p.when,
+                offset=self.p.offset,
+                repeat=self.p.repeat,
+                weekdays=self.p.weekdays,
+            )
+        if self.p.cheat:
+            self.add_timer(
+                when=self.p.when,
+                offset=self.p.offset,
+                repeat=self.p.repeat,
+                cheat=True,
+            )
+
+        self.order = None
+
+    def prenext(self):
+        self.next()
+
+    def next(self):
+        _, isowk, isowkday = self.datetime.date().isocalendar()
+        txt = '{}, {}, Week {}, Day {}, O {}, H {}, L {}, C {}'.format(
+            len(self), self.datetime.datetime(),
+            isowk, isowkday,
+            self.data.open[0], self.data.high[0],
+            self.data.low[0], self.data.close[0])
+
+        print(txt)
+
+    def notify_timer(self, timer, when, *args, **kwargs):
+        print('strategy notify_timer with tid {}, when {} cheat {}'.
+              format(timer.p.tid, when, timer.p.cheat))
+
+        if self.order is None and timer.p.cheat:
+            print('-- {} Create buy order'.format(self.data.datetime.date()))
+            self.order = self.buy()
+
+    def notify_order(self, order):
+        if order.status == order.Completed:
+            print('-- {} Buy Exec @ {}'.format(
+                self.data.datetime.date(), order.executed.price))
+
+
+def runstrat(args=None):
+    args = parse_args(args)
+
+    cerebro = bt.Cerebro()
+
+    # Data feed kwargs
+    kwargs = dict(
+        timeframe=bt.TimeFrame.Days,
+        compression=1,
+        sessionstart=datetime.time(9, 0),
+        sessionend=datetime.time(17, 30),
+    )
+
+    # Parse from/to-date
+    dtfmt, tmfmt = '%Y-%m-%d', 'T%H:%M:%S'
+    for a, d in ((getattr(args, x), x) for x in ['fromdate', 'todate']):
+        if a:
+            strpfmt = dtfmt + tmfmt * ('T' in a)
+            kwargs[d] = datetime.datetime.strptime(a, strpfmt)
+
+    # Data feed
+    data0 = bt.feeds.BacktraderCSVData(dataname=args.data0, **kwargs)
+    cerebro.adddata(data0)
+
+    # Broker
+    cerebro.broker = bt.brokers.BackBroker(**eval('dict(' + args.broker + ')'))
+
+    # Sizer
+    cerebro.addsizer(bt.sizers.FixedSize, **eval('dict(' + args.sizer + ')'))
+
+    # Strategy
+    cerebro.addstrategy(St, **eval('dict(' + args.strat + ')'))
+
+    # Execute
+    cerebro.run(**eval('dict(' + args.cerebro + ')'))
+
+    if args.plot:  # Plot if requested to
+        cerebro.plot(**eval('dict(' + args.plot + ')'))
+
+
+def parse_args(pargs=None):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=(
+            'Sample Skeleton'
+        )
+    )
+
+    parser.add_argument('--data0', default='../../datas/2005-2006-day-001.txt',
+                        required=False, help='Data to read in')
+
+    # Defaults for dates
+    parser.add_argument('--fromdate', required=False, default='',
+                        help='Date[time] in YYYY-MM-DD[THH:MM:SS] format')
+
+    parser.add_argument('--todate', required=False, default='',
+                        help='Date[time] in YYYY-MM-DD[THH:MM:SS] format')
+
+    parser.add_argument('--cerebro', required=False, default='',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--broker', required=False, default='',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--sizer', required=False, default='',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--strat', required=False, default='',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--plot', required=False, default='',
+                        nargs='?', const='{}',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    return parser.parse_args(pargs)
+
+
+if __name__ == '__main__':
+    runstrat()
