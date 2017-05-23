031480712e65b2635eacc95a9dd04cd02ae50b8a
mementum <mementum@users.noreply.github.com>
2017-05-23 23:36:13 +0200

    Add documentation and parametrize all values in TrueStrengthIndicator

--- backtrader/indicators/tsi.py
+++ backtrader/indicators/tsi.py
@@ -27,20 +27,48 @@ from . import EMA
 
 
 class TrueStrengthIndicator(bt.Indicator):
+    '''
+    The True Strength Indicators was first introduced in Stocks & Commodities
+    Magazine by its author William Blau. It measures momentum with a double
+    exponential (default) of the prices.
+
+    It shows divergence if the extremes keep on growign but closing prices
+    do not in the same manner (distance to the extremes grow)
+
+    Formula:
+      - price_change = close - close(pchange periods ago)
+      - sm1_simple = EMA(price_close_change, period1)
+      - sm1_double = EMA(sm1_simple, period2)
+      - sm2_simple = EMA(abs(price_close_change), period1)
+      - sm2_double = EMA(sm2_simple, period2)
+      - tsi = 100.0 * sm1_double / sm2_double
+
+    See:
+      - http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:true_strength_index
+
+    Params
+
+      - ``period1``: the period for the 1st smoothing
+      - ``period2``: the period for the 2nd smoothing
+      - ``pchange``: the lookback period for the price change
+      - ``_movav``: the moving average to apply for the smoothing
+    '''
     alias = ('TSI',)
     params = (
         ('period1', 25),
         ('period2', 13),
+        ('pchange', 1),
+        ('_movav', EMA),
     )
     lines = ('tsi',)
 
     def __init__(self):
-        pc = self.data - self.data(-1)
+        pc = self.data - self.data(-self.p.pchange)
 
-        sm1 = bt.ind.EMA(pc, period=self.p.period1)
-        sm12 = bt.ind.EMA(sm1, period=self.p.period2)
+        sm1 = self.p._movav(pc, period=self.p.period1)
+        sm12 = self.p._movav(sm1, period=self.p.period2)
 
-        sm2 = bt.ind.EMA(abs(pc), period=self.p.period1)
-        sm22 = bt.ind.EMA(sm2, period=self.p.period2)
+        sm2 = self.p._movav(abs(pc), period=self.p.period1)
+        sm22 = self.p._movav(sm2, period=self.p.period2)
 
         self.lines.tsi = 100.0 * (sm12 / sm22)
