b1896429d943c8e616fc98611e4cdd782e203117
mementum <mementum@users.noreply.github.com>
2017-06-11 23:10:31 +0200

    Generalize AccelerationDecelerationOscillator and add test

--- backtrader/indicators/accdecoscillator.py
+++ backtrader/indicators/accdecoscillator.py
@@ -22,34 +22,38 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import backtrader as bt
-from . import SMA
-from . import AO
+from . import MovAv, AwesomeOscillator
+
+
+__all__ = ['AccelerationDecelerationOscillator', 'AccDeOsc']
 
 
 class AccelerationDecelerationOscillator(bt.Indicator):
     '''
-    Acceleration/Deceleration Technical Indicator (AC) measures acceleration and
-    deceleration of the current driving force. This indicator will change
+    Acceleration/Deceleration Technical Indicator (AC) measures acceleration
+    and deceleration of the current driving force. This indicator will change
     direction before any changes in the driving force, which, it its turn, will
     change its direction before the price.
 
     Formula:
-     - MEDIAN PRICE = (HIGH + LOW) / 2
-     - AO = SMA (MEDIAN PRICE, 5) - SMA (MEDIAN PRICE, 34)
-     - AC = AO - SMA (AO, 5)
+     - AcdDecOsc = AwesomeOscillator - SMA(AwesomeOscillator, period)
 
     See:
       - https://www.metatrader5.com/en/terminal/help/indicators/bw_indicators/ao
       - https://www.ifcmarkets.com/en/ntx-indicators/ntx-indicators-accelerator-decelerator-oscillator
 
     '''
-    alias = ('AC', )
-    lines = ('ac', )
+    alias = ('AccDeOsc',)
+    lines = ('accde', )
 
-    plotlines = dict(ac=dict(_method='bar'))
+    params = (
+        ('period', 5),
+        ('movav', MovAv.SMA),
+    )
+
+    plotlines = dict(accde=dict(_method='bar', alpha=0.50, width=1.0))
 
     def __init__(self):
-
-        ao = AO()
-        #self.l.ac = ao - SMA(ao, 5)
-        self.l.ac = ao - SMA(ao, period=5)
+        ao = AwesomeOscillator()
+        self.l.accde = ao - self.p.movav(ao, period=self.p.period)
+        super(AccelerationDecelerationOscillator, self).__init__()
