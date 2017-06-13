0d7abbe8aa1741b8d8ea57dbd0d3d9f4be8a3eda
mementum <mementum@users.noreply.github.com>
2017-06-13 20:33:22 +0200

    Add notes about usage of Hurst exponent and lag_start/lag_end parameters to override default lag values

--- backtrader/indicators/hurst.py
+++ backtrader/indicators/hurst.py
@@ -39,6 +39,22 @@ class HurstExponent(PeriodN):
       1. Geometric random walk (H=0.5)
       2. Mean-reverting series (H<0.5)
       3. Trending Series (H>0.5)
+
+    Important notes:
+
+      - The default period is ``40``, but experimentation by users has shown
+        that it would be advisable to have at least 2000 samples (i.e.: a
+        period of at least 2000) to have stable values.
+
+      - The `lag_start` and `lag_end` values will default to be ``2`` and
+        ``self.p.period / 2`` unless the parameters are specified.
+
+        Experimentation by users has also shown that values of around ``10``
+        and ``500`` produce good results
+
+    The original values (40, 2, self.p.period / 2) are kept for backwards
+    compatibility
+
     '''
     frompackages = (
         ('numpy', ('asarray', 'log10', 'polyfit', 'sqrt', 'std', 'subtract')),
@@ -46,12 +62,24 @@ class HurstExponent(PeriodN):
 
     alias = ('Hurst',)
     lines = ('hurst',)
-    params = (('period', 40),)
+    params = (
+        ('period', 40),  # 2000 was proposed
+        ('lag_start', None),  # 10 was proposed
+        ('lag_end', None),  # 500 was proposed
+    )
+
+    def _plotlabel(self):
+        plabels = [self.p.period]
+        plabels += [self._lag_start]
+        plabels += [self._lag_end]
+        return plabels
 
     def __init__(self):
         super(HurstExponent, self).__init__()
         # Prepare the lags array
-        self.lags = asarray(range(2, self.p.period // 2))
+        self._lag_start = lag_start = self.p.lag_start or 2
+        self._lag_end = lag_end = self.p.lag_end or (self.p.period // 2)
+        self.lags = asarray(range(lag_start, lag_end))
         self.log10lags = log10(self.lags)
 
     def next(self):
