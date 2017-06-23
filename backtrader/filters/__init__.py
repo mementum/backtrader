32b070274fc84ca5a98da0c2bed094bce87c50c4
mementum <mementum@users.noreply.github.com>
2017-06-24 00:00:46 +0200

    Add a Renko filter for data feeds

--- backtrader/filters/__init__.py
+++ backtrader/filters/__init__.py
@@ -22,6 +22,8 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 
+from .. import Filter
+
 from .datafilter import *
 from .datafiller import *
 from .session import *
@@ -29,3 +31,4 @@ from .calendardays import *
 from .daysteps import *
 from .bsplitter import *
 from .heikinashi import *
+from .renko import *
