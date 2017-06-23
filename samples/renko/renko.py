2664a6759285da60ce5094f3a4a6ee1a91ce0d0e
mementum <mementum@users.noreply.github.com>
2017-06-24 00:20:22 +0200

    Add Renko filter sample

---
+++ samples/renko/renko.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2017 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import argparse
+import datetime
+
+import backtrader as bt
+
+
+class St(bt.Strategy):
+    params = dict(
+    )
+
+    def __init__(self):
+        for d in self.datas:
+            bt.ind.RSI(d)
+
+    def next(self):
+        pass
+
+
+def runstrat(args=None):
+    args = parse_args(args)
+
+    cerebro = bt.Cerebro()
+
+    # Data feed kwargs
+    kwargs = dict()
+
+    # Parse from/to-date
+    dtfmt, tmfmt = '%Y-%m-%d', 'T%H:%M:%S'
+    for a, d in ((getattr(args, x), x) for x in ['fromdate', 'todate']):
+        if a:
+            strpfmt = dtfmt + tmfmt * ('T' in a)
+            kwargs[d] = datetime.datetime.strptime(a, strpfmt)
+
+    data0 = bt.feeds.BacktraderCSVData(dataname=args.data0, **kwargs)
+
+    fkwargs = dict()
+    fkwargs.update(**eval('dict(' + args.renko + ')'))
+
+    if not args.dual:
+        data0.addfilter(bt.filters.Renko, **fkwargs)
+        cerebro.adddata(data0)
+    else:
+        cerebro.adddata(data0)
+        data1 = data0.clone()
+        data1.addfilter(bt.filters.Renko, **fkwargs)
+        cerebro.adddata(data1)
+
+    # Broker
+    cerebro.broker = bt.brokers.BackBroker(**eval('dict(' + args.broker + ')'))
+
+    # Sizer
+    cerebro.addsizer(bt.sizers.FixedSize, **eval('dict(' + args.sizer + ')'))
+
+    # Strategy
+    cerebro.addstrategy(St, **eval('dict(' + args.strat + ')'))
+
+    # Execute
+    kwargs = dict(stdstats=False)
+    kwargs.update(**eval('dict(' + args.cerebro + ')'))
+    cerebro.run(**kwargs)
+
+    if args.plot:  # Plot if requested to
+        kwargs = dict(style='candle')
+        kwargs.update(**eval('dict(' + args.plot + ')'))
+        cerebro.plot(**kwargs)
+
+
+def parse_args(pargs=None):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=(
+            'Renko bricks sample'
+        )
+    )
+
+    parser.add_argument('--data0', default='../../datas/2005-2006-day-001.txt',
+                        required=False, help='Data to read in')
+
+    # Defaults for dates
+    parser.add_argument('--fromdate', required=False, default='',
+                        help='Date[time] in YYYY-MM-DD[THH:MM:SS] format')
+
+    parser.add_argument('--todate', required=False, default='',
+                        help='Date[time] in YYYY-MM-DD[THH:MM:SS] format')
+
+    parser.add_argument('--cerebro', required=False, default='',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--broker', required=False, default='',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--sizer', required=False, default='',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--strat', required=False, default='',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--plot', required=False, default='',
+                        nargs='?', const='{}',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--renko', required=False, default='',
+                        metavar='kwargs', help='kwargs in key=value format')
+
+    parser.add_argument('--dual', required=False, action='store_true',
+                        help='put the filter on a second version of the data')
+
+    return parser.parse_args(pargs)
+
+
+if __name__ == '__main__':
+    runstrat()
