5c6ec8a409a809c1093e9abfa1ecc01f73747f37
mementum <mementum@users.noreply.github.com>
2017-07-04 07:12:42 +0200

    Add abstract method add_order_history

--- backtrader/broker.py
+++ backtrader/broker.py
@@ -66,6 +66,10 @@ class BrokerBase(with_metaclass(MetaBroker, object)):
     def stop(self):
         pass
 
+    def add_order_history(self, orders, notify=False):
+        '''Add order history. See cerebro for details'''
+        raise NotImplementedError
+
     def getcommissioninfo(self, data):
         '''Retrieves the ``CommissionInfo`` scheme associated with the given
         ``data``'''
