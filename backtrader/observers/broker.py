d68287abbcb307d0c90e40e04d66a543431a6eb7
mementum <mementum@users.noreply.github.com>
2017-07-08 01:17:22 +0200

    Autoadapt broker observer to fundmode

--- backtrader/observers/broker.py
+++ backtrader/observers/broker.py
@@ -86,14 +86,30 @@ class Broker(Observer):
     '''
     _stclock = True
 
+    params = (
+        ('fund', None),
+    )
+
     alias = ('CashValue',)
     lines = ('cash', 'value')
 
     plotinfo = dict(plot=True, subplot=True)
 
+    def start(self):
+        if self.p.fund is None:
+            self._fundmode = self._owner.broker.fundmode
+        else:
+            self._fundmode = self.p.fund
+
+        if self._fundmode:
+            self.plotlines.value._plotskip = True
+
     def next(self):
-        self.lines.cash[0] = self._owner.broker.getcash()
-        self.lines.value[0] = value = self._owner.broker.getvalue()
+        if not self._fundmode:
+            self.lines.value[0] = value = self._owner.broker.getvalue()
+            self.lines.cash[0] = self._owner.broker.getcash()
+        else:
+            self.lines[0][0] = self._owner.broker.fundvalue
 
 
 class FundValue(Observer):
