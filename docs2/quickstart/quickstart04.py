7789678ec0671b19a7e002300ba732cc57e3cbf6
Magnus Wahlberg <endast@gmail.com>
2017-07-25 14:55:43 +0200

    Typo

--- docs2/quickstart/quickstart04.py
+++ docs2/quickstart/quickstart04.py
@@ -33,7 +33,7 @@ import backtrader as bt
 class TestStrategy(bt.Strategy):
 
     def log(self, txt, dt=None):
-        ''' Logging function fot this strategy'''
+        ''' Logging function for this strategy'''
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
 
