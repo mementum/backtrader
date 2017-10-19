e475953a3ed7e935e256a6be6e0799acf3e48775
Min Sik Kim <msk@dolbo.net>
2017-10-19 10:13:16 -0700

    Fix syntax errors in datacache.py

--- backtrader/datacache.py
+++ backtrader/datacache.py
@@ -55,7 +55,7 @@ class BaseCache(bt.LineSeries):
 
 
 if True:
-    impor os
+    import os
     import os.path
     import sqlite3
 
@@ -77,7 +77,7 @@ def CacheSQLite(BaseCache):
 
         else:
             path = os.getenv('XDG_CACHE_HOME', os.path.expanduser('~/.cache'))
-            path os.path.join(path, self.p.appname)
+            path = os.path.join(path, self.p.appname)
 
         path = os.path.join(path, self.p.location,
                             bt.TimeFrame.TName(self.p.timeframe),
