13941fd603e5483f01fb03988430c8e250420ad5
backtrader <backtrader@users.noreply.github.com>
2018-01-26 00:29:56 +0100

    Release 1.9.60.122

--- backtrader/version.py
+++ backtrader/version.py
@@ -22,6 +22,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 
-__version__ = '1.9.59.122'
+__version__ = '1.9.60.122'
 
 __btversion__ = tuple(int(x) for x in __version__.split('.'))
