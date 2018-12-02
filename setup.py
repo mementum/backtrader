c0e31c5e9331258c40db01b9b6b7b73eb9b6dfd8
Dima Berastau <dima.berastau@gmail.com>
2018-12-01 17:23:24 -0800

    Explicitly specifying requirements.

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setuptools.setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=[],
+    install_requires=[],
 
     # List additional groups of dependencies here
     # (e.g. development dependencies).
