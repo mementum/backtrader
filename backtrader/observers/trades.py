f83cf94270459e071ecf86bf8772889af4852e09
backtrader <backtrader@users.noreply.github.com>
2019-04-27 07:15:27 +0200

    Trades observer to show net profit instead of brutto, with parameter to control behavior

--- backtrader/observers/trades.py
+++ backtrader/observers/trades.py
@@ -37,11 +37,17 @@ class Trades(Observer):
     is then closed when it goes back to 0 (or crosses over 0 in the opposite
     direction)
 
-    Params: None
+    Params:
+      - ``pnlcomm`` (def: ``True``)
+
+        Show net/profit and loss, i.e.: after commission. If set to ``False``
+        if will show the result of trades before commission
     '''
     _stclock = True
 
     lines = ('pnlplus', 'pnlminus')
+
+    params = dict(pnlcomm=True)
 
     plotinfo = dict(plot=True, subplot=True,
                     plotname='Trades - Net Profit/Loss',
@@ -90,10 +96,12 @@ class Trades(Observer):
             if not trade.isclosed:
                 continue
 
-            if trade.pnl >= 0:
-                self.lines.pnlplus[0] = trade.pnl
+            pnl = trade.pnlcomm if self.p.pnlcomm else trade.pnl
+
+            if pnl >= 0.0:
+                self.lines.pnlplus[0] = pnl
             else:
-                self.lines.pnlminus[0] = trade.pnl
+                self.lines.pnlminus[0] = pnl
 
 
 class MetaDataTrades(Observer.__class__):
