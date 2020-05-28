db517da753a6c393e17e39ca44605c818b49bddf
backtrader <backtrader@users.noreply.github.com>
2020-05-28 05:12:47 +0200

    Remove Python 2 and some travis test versions below 3.6

--- setup.py
+++ setup.py
@@ -84,8 +84,6 @@ setuptools.setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
