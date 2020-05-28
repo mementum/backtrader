5cbc8ff8748512e2fa6c5956452e65432a737a8e
backtrader <backtrader@users.noreply.github.com>
2020-05-28 04:22:49 +0200

    Fixed backend loading if a backend is loaded (Google Collab) and backend to use on MacOSX

--- backtrader/plot/__init__.py
+++ backtrader/plot/__init__.py
@@ -21,13 +21,22 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import sys
+
+
 try:
     import matplotlib
 except ImportError:
     raise ImportError(
         'Matplotlib seems to be missing. Needed for plotting support')
 else:
-    matplotlib.use('TkAgg')
+    touse = 'TKAgg' if sys.platform != 'darwin' else 'MacOSX'
+    try:
+        matplotlib.use(touse)
+    except:
+        # if another backend has already been loaded, an exception will be
+        # generated and this can be skipped
+        pass
 
 
 from .plot import Plot, Plot_OldSync
