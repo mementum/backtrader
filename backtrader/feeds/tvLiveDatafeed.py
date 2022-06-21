451a4a3afdb868a7a5e483480e888e3c4ba54aa2
Ron Freimann <ronfreimann@gmail.com>
2022-06-21 11:48:09 +0300

    Asset adding is done by the caller now, this module only adds callback to that asset set

--- backtrader/feeds/tvLiveDatafeed.py
+++ backtrader/feeds/tvLiveDatafeed.py
@@ -1,14 +1,18 @@
+'''
+TODO:
+
+'''
+
 import queue
 from backtrader import date2num
 from backtrader.feeds import DataBase
 
 class tvLiveDatafeed(DataBase): # PARENT class is AbstractDataBase
 
-    def __init__(self, tv_datafeed, symbol, exchange, interval):
+    def __init__(self, tv_datafeed_realtime, asset_id):
         self.newData = False # flag to indicate if data in queue available; set by receive_data and cleared by _load only when queue is empty
-        self.tdr=tv_datafeed
-        self.asset_id=self.tdr.add_symbol(symbol, exchange, interval) # add symbol into set of symbols we monitor and collect data for; if already added then existing asset_id will be returned
-        self.tdr.add_callback(self.asset_id, self.receive_data) # add callback method for this symbol
+        self.tdr=tv_datafeed_realtime
+        self.callback_id=self.tdr.add_callback(asset_id, self.receive_data) # add callback method for this asset set; keep callback id so later we can remove it
         self.data_queue=queue.Queue() # queue where all the new data is put for the _load() method to process
         
     def start(self):
@@ -17,11 +21,17 @@ class tvLiveDatafeed(DataBase): # PARENT class is AbstractDataBase
     def receive_data(self, data): # this function is called by the tvDatafeedRealtime whenever the symbol we use in this datafeed has a new value
         self.data_queue.put(data) # put new data in to queue
         self.newData=True # set the flag so _load() knows there is new data
+    
+    def remove_callback(self):
+        self.tdr.del_callback(self.callback_id)
 
     def _load(self):  # this is called inside the load() method in AbstractDataBase and will actually assign data to lines
         if self.newData:
             data=self.data_queue.get() # get next available data bar
-            # add new data togetehr with its datetime into lines
+            if isinstance(data,str): # if string is received then that is "EXIT"
+                return False # means user stopped this strategy
+            
+            # add new data together with its datetime into lines
             self.lines.datetime[0] = date2num(data.index[0])
             # Get the rest of the unpacked data
             self.lines.open[0] = data.open[0]
@@ -46,4 +56,3 @@ class tvLiveDatafeed(DataBase): # PARENT class is AbstractDataBase
     def stop(self):
         '''Stops and tells the store to stop'''
         super().stop() # calls PARENT class (AbstractDataBase) stop() method
-        
